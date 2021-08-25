7258894dcd1c0fb204bd025fb3404d3d6d4d214c
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-25 23:45:05 +0700

    Add Head component with meta tags, google tag manager

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import "twind/shim"
   import { tw } from "twind"
   import logo from "./assets/svelte.png"
+  import Head from "./lib/Head.svelte"
   import Kofi from "./lib/Kofi.svelte"
   import Menu from "./lib/Menu.svelte"
   import Social from "./lib/Social.svelte"
@@ -10,11 +11,17 @@
   const title = "Single Page Svelte"
 
   const menuItems = [{ name: "Github", url: "https://github.com/narze/single-page-svelte" }]
+
+  const description = "Build a single page app with Svelte, quickly."
+  const imageUrl =
+    "https://raw.githubusercontent.com/narze/timelapse/master/projects/single-page-svelte_home.png"
+  const gtagId = null
 </script>
 
 <Kofi name="narze" label="Support Me" />
 <Menu items={menuItems} />
 <Social {url} {title} />
+<Head {title} {description} {url} {imageUrl} {gtagId} />
 
 <main class="w-full h-screen flex flex-col justify-center items-center">
   <h1 class="text-6xl text-green-400 flex flex-col">
